1041f99d572e9547605fb83750af3365f6ef4173
EugeneTeam <wtf.evgesha@gmail.com>
2021-12-11 14:43:01 +0200

    added models to prisma schema: -PaymentType -DeliveryService -Order added ts interfaces

--- src/graphql/types/test.ts
+++ src/graphql/types/test.ts
@@ -1,13 +1,16 @@
 import {gql} from 'apollo-server-express';
 import {PrismaClient} from '@prisma/client';
 
+import {IDeliveryServices, IPaymentType} from '../typescript/interfaces'
+
 export default class Test {
     static resolver() {
         return {
             Query: {
                 testQuery: async (obj: any, args: any, context: any) => {
-                    const prisma = new PrismaClient()
-                    console.log(await prisma.user.findMany())
+                    const prisma: PrismaClient = new PrismaClient()
+                    const temp: Array<IPaymentType> = await prisma.deliveryService.findMany()
+                    console.log(temp)
                     return {
                         status: 'SUCCESS',
                         message: 'TEST ROUTE!'
---
+++ src/graphql/typescript/enums.ts
@@ -0,0 +1,5 @@
+export enum USER_STATE {
+    ACTIVE,
+    INACTIVE,
+    BANNED
+}
